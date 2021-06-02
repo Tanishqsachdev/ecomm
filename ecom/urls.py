f5a9533d7ab965cb885b6e5b2c1e703612ac8c72
Tanishq Sachdev <tanishqsachdev76@gmail.com>
2021-06-02 23:35:15 +0530

    Password Reset Views Added, Password Reset Complete

--- ecom/urls.py
+++ ecom/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.contrib.auth import views as auth_views
 from main.views import *
 from django.conf.urls.static import static
 from django.conf import settings
@@ -40,7 +41,11 @@ urlpatterns = [
     path('checkout/', Checkout.as_view(),name='checkout'),
     path('order/', OrderView.as_view(),name='order'),
     path('order/<pk>', OrderDetails.as_view(),name='order_details'),
-    
-    
+    #password_reset
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html"),name='password_reset'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"),name='password_reset_confirm'),
+    path('reset_password/done/',auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"),name='password_reset_done'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"),name='password_reset_complete'),
+  
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
